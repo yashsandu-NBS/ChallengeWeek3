d2094456b7d44bf49c3dfbc7454409faf129e2db
yashsandu-nw <71331116+yashsandu-nw@users.noreply.github.com>
2020-10-02 13:40:40 +0100

    added comments

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 
+#Starting up the container
 sudo docker run -d -p 5000:5000 --name python-app localhost:8082/python-app
+
+# Going to the directory where app.py is located
 /home/student/nbs-challenge-3/
+
+# Running the test
 py.test
+
+# Deleting the container, ready for next time
 sudo docker rm -f python-app
