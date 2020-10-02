6f6c27ebaff1fd017dfa15e5789845a3ec1a6c28
yashsandu-nw <yash.sandu@nationwide.co.uk>
2020-10-02 10:42:25 +0100

    moved app.py and pytest to push.sh

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
 cd /home/student/nbs-challenge-3/
-pip install -r requirements.txt
-pytest
+pip install -r requirements
 sudo docker build -t localhost:8082/python-app .
--- scripts/push_images.sh
+++ scripts/push_images.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
 sudo docker push localhost:8082/python-app
+pytest
+python3 app.py
+
