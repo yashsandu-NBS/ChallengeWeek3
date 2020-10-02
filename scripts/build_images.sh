c4fd56b3b0cc2030a2438b3c53cef4c97b1d4d5d
yashsandu-nw <71331116+yashsandu-nw@users.noreply.github.com>
2020-10-02 13:45:05 +0100

    Update build_images.sh

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+# Full path to the requirements.txt
 cd /home/student/nbs-challenge-3/
+
+# Installing the requirments.txt file
 pip install -r requirements
+
+# Building the image
 sudo docker build -t localhost:8082/python-app .
